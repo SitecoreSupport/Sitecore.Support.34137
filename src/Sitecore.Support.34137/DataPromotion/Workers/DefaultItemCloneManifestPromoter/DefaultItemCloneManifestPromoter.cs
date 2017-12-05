da2d18367d991f51a30884858ee87528a8eef8fa
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2017-12-05 23:13:07 +0200

    Import DefaultItemCloneManifestPromoter.MergeCloneAndSourceVariants() code

--- src/Sitecore.Support.34137/DataPromotion/Workers/DefaultItemCloneManifestPromoter/DefaultItemCloneManifestPromoter.cs
+++ src/Sitecore.Support.34137/DataPromotion/Workers/DefaultItemCloneManifestPromoter/DefaultItemCloneManifestPromoter.cs
@@ -147,5 +147,70 @@ namespace Sitecore.Support.Framework.Publishing.DataPromotion
         .Where(x => x != null)
         .ToArray();
     }
+
+    protected virtual Tuple<IItemVariant, IItemRelationship[]> MergeCloneAndSourceVariants(
+      IItemVariant cloneVariant,
+      IEnumerable<IItemRelationship> cloneRelationships,
+      IItemVariant sourceVariant,
+      IEnumerable<IItemRelationship> sourceRelationships)
+    {
+      // item data
+      var itemData = cloneVariant.Properties;
+
+      // fields data
+      var fieldsData = cloneVariant.Fields
+        // remove the clone (source) fields
+        .Where(x => x.FieldId != PublishingConstants.Clones.SourceItem &&
+                    x.FieldId != PublishingConstants.Clones.SourceVariant)
+        .Concat(
+          // only use the fields from source if they don't exist in the clone
+          sourceVariant.Fields
+            .Where(x => cloneVariant.Fields.All(c => c.FieldId != x.FieldId))
+            .Select(x => new FieldData(
+              x.FieldId,
+              cloneVariant.Id,
+              x.RawValue,
+              CreateFieldVarianceInfo(cloneVariant, x))))
+        .ToArray();
+
+      // links data
+      var linksFromClone = cloneRelationships
+        .Where(cloneRel =>
+          // this is a special relationship that captures the link between an item and its template
+          // this needs to be processed once .. hence it's being taken from the source and skiped from the clone (both must have the same template id)
+            cloneRel.Type != ItemRelationshipType.TemplatedBy &&
+            !cloneVariant.Fields.Any() ||
+            cloneRel.SourceFieldId == null ||
+            cloneVariant.Fields.Any(c => c.FieldId == cloneRel.SourceFieldId))
+        .ToArray();
+
+      var linksFromSource = sourceRelationships
+        .Select(x => new ItemRelationship(
+          Guid.NewGuid(), // generate a new id to avoid database unique key constraint as all items from all targets are saved in the same links table!
+          x.SourceId == sourceVariant.Id ? cloneVariant.Id : x.SourceId, // replace the sourceId with the cloned item id
+          x.TargetId,
+          x.Type,
+          x.SourceVariance,
+          x.TargetVariance, x.TargetPath, x.SourceFieldId))
+        .ToArray();
+
+      // use the links from clone + any additional links from the source
+      var linksData = linksFromClone
+        .Concat(linksFromSource.Where(x =>
+          !linksFromClone.Any(s => s.SourceId == x.SourceId &&
+                                   s.SourceFieldId == x.SourceFieldId &&
+                                   x.Type == s.Type)))
+        .ToArray();
+
+      return new Tuple<IItemVariant, IItemRelationship[]>(
+        new ItemVariant(
+          cloneVariant.Id,
+          cloneVariant.Language,
+          cloneVariant.Version,
+          cloneVariant.Revision,
+          itemData,
+          fieldsData),
+        linksData);
+    }
   }
 }
